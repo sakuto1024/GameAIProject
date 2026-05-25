51f2ebaaa8e70f9b3137dd3adc355dce709f5f2a
咲音 上関 <250323@jc-21.jp>
2026-05-25 11:28:51 +0900

    一応、パンダがプレイヤーを追ってくるようにはした。間に壁があったとき、壁につっかえて動かなくなってしまう

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,15 +1,18 @@
 #include "Enemy.h"
 #include "time.h"
 #include "Stage.h"
+#include "Player.h"
 
 namespace
 {
 	const int ENEMY_SIZE = 48; //敵のサイズ 32*32
 	const Point ENEMY_START_POS = { 20 * ENEMY_SIZE, 10 * ENEMY_SIZE }; //敵の初期位置
-	const DIR INIT_ENEMY_DIR = { LEFT };
+	const DIR INIT_ENEMY_DIR = { DOWN };
 	const int ENEMY_DRAW_SIZE = 32; //敵の描画サイズ
 	const int animFrame[4]{ 0, 1, 2, 1 };
 	const float ANIM_INTERVAL = 0.2f;
+
+	int mapData[];
 }
 
 
@@ -34,11 +37,11 @@ void Enemy::Update()
 	float dt = Time::DeltaTime();
 	dir_timer = dir_timer - dt;
 	prog_timer = prog_timer - dt;
-	if (dir_timer < 0.0f)
+	/*if (dir_timer < 0.0f)
 	{
 		dir_ = (DIR)(GetRand(3));
 		dir_timer = 3.0f + dir_timer;
-	}
+	}*/
 
 	Point newPos = pos_;
 	if (prog_timer < 0.0f)
@@ -61,9 +64,9 @@ void Enemy::Update()
 			break;
 		}
 
-		int mapValue = FindGameObject<Stage>()->GetMap(newPos.x / CHA_SIZE, newPos.y / CHA_SIZE);
-		//Stage* stage = FindGameObject<Stage>();
-		// int mapValue = stage->>GetMap(newPos.x / CHA_SIZE, newPos.y / CHA_SIZE);
+		//int mapValue = FindGameObject<Stage>()->GetMap(newPos.x / CHA_SIZE, newPos.y / CHA_SIZE);
+		Stage* stage = FindGameObject<Stage>();
+		 int mapValue = stage->GetMap(newPos.x / CHA_SIZE, newPos.y / CHA_SIZE);
 		//移動先がステージの外に出ないようにする
 		if (mapValue != 1)
 		{
@@ -72,8 +75,58 @@ void Enemy::Update()
 
 		else if (mapValue == 1)
 		{
-			dir_ = (DIR)(GetRand(3));
+			if (dir_ == UP || dir_ == DOWN)
+			{
+				dir_ = (DIR)(GetRand(1) * 2 + 1); 
+			}
+			else if (dir_ == LEFT || dir_ == RIGHT)
+			{
+				dir_ = (DIR)(GetRand(1) * 2); 
+			}
 		}
+
+		Player* player = FindGameObject<Player>();
+		Point pPos = player->GetPlayerPos();
+
+		float distanceX = (pPos.x - pos_.x) * (pPos.x - pos_.x);
+		float distanceY = (pPos.y - pos_.y) * (pPos.y - pos_.y);
+		float distance = distanceX + distanceY;
+
+		float radius = (128.0f + 128.0f) * (128.0f + 128.0f);
+
+		if (distance < radius)
+		{
+			DrawString(10, 700, "検知", GetColor(0, 0, 0));
+
+			if (distanceX < distanceY)
+			{
+				if ((pPos.y - pos_.y) > 0)
+				{
+					dir_ = DOWN;
+				}
+				else
+				{
+					dir_ = UP;
+				}
+			}
+
+			else
+			{
+				if ((pPos.x - pos_.x) > 0)
+				{
+					dir_ = RIGHT;
+				}
+				else
+				{
+					dir_ = LEFT;
+				}
+			}
+		}
+		
+
+		//DrawCircle(pos_.x + 16, pos_.y, 32 + 16, GetColor(0, 255, 0), TRUE);
+
+
 
 		/*if (!(newPos.x < 1 || newPos.x >(STAGE_WIDTH - 2) * ENEMY_DRAW_SIZE
 			|| newPos.y < 1 || newPos.y >(STAGE_HEIGHT - 2) * ENEMY_DRAW_SIZE))
